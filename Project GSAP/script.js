cd37a30387691c00e9a3c7d7e5ac07d85cb41748
Aditya <2100330100015@rkgit.edu.in>
2024-12-13 00:17:09 +0530

    Made loader successfully

---
+++ Project GSAP/script.js
@@ -0,0 +1,25 @@
+var tl = gsap.timeline()
+
+tl.from("#nav h3", {
+    y: -50,
+    opacity:0,
+    delay:0.4,
+    duration:0.8,
+    stagger:0.2
+})
+
+tl.from("#main h1", {
+    x:-500,
+    opacity:0,
+    duration:0.8,
+    stagger:0.4   //sari cheeze ek - ek karke aayengi. stagger ki help se
+})
+
+tl.from("img",{
+    x:100,
+    rotate:45,
+    opacity:0,
+    duration:1,
+    stagger:0.5,
+    
+})
